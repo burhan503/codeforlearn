f5fd88bb8f965c083fea3b89f5b23e58a8f5debb
Burhan <91361396+burhan503@users.noreply.github.com>
2023-12-06 06:09:17 +0000

    forloop string printing program

--- forloop.js
+++ forloop.js
@@ -1,1 +1,10 @@
-console.log("hello tesing")
+let number = 10;
+let string = 0;
+
+for(let i = 0; i<=number; i++){
+    for(let j=0; j<=i; j++){
+        string += "*"
+    }
+    string += "/n "
+}
+console.log(string)
